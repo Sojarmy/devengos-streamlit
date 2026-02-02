c514738c4495fbff3364d3641bf5eb73f62c4656
Sojarmy <ulisespec@gmail.com>
2026-02-02 13:31:12 +0000

    Mostrar UI en Streamlit

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("app.py OK")
+
+import streamlit as st
+
+st.set_page_config(page_title="DevengosCuentas2026", layout="wide")
+st.title("DevengosCuentas2026")
+st.success("✅ Streamlit está leyendo este app.py")
+st.write("Si ves esto, todo funciona.")
